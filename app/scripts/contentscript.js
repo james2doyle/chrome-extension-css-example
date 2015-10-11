d445841ec8718b186f610b69e1c150d7123ac29f
james2doyle <james2doyle@gmail.com>
2015-10-11 14:56:51 -0700

    add option for toggle of stylesheet removal

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,18 +1,26 @@
 'use strict';
 
-// remove all stylesheets
-Array.prototype
-.slice.call(document.documentElement.querySelectorAll('link'), 0)
-.forEach(function(el) {
-  if(el.type && el.type === 'text/css') {
-    el.parentNode.removeChild(el);
+// inject the stylesheet that is in the options page
+chrome.storage.sync.get('cssremove', function(item) {
+  if (item.cssremove) {
+    // remove all stylesheets
+    Array.prototype
+    .slice.call(document.documentElement.querySelectorAll('link'), 0)
+    .forEach(function(el) {
+      if(el.type && el.type === 'text/css') {
+        el.parentNode.removeChild(el);
+      }
+    });
   }
 });
 
 // inject the stylesheet that is in the options page
 chrome.storage.sync.get('cssurl', function(item) {
-  var style = document.createElement('link');
-  style.rel = 'stylesheet';
-  style.href = item.cssurl;
-  document.head.appendChild(style);
+  // dont create empty link elements
+  if (item.cssurl !== '') {
+    var style = document.createElement('link');
+    style.rel = 'stylesheet';
+    style.href = item.cssurl;
+    document.head.appendChild(style);
+  }
 });
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -5,7 +5,12 @@ function save(e) {
   clearTimeout(window.delayer);
   window.delayer = setTimeout(function() {
     var item = {};
-    item[e.target.id] = e.target.value;
+    // checkboxes store the checked state
+    if (e.target.type === 'checkbox') {
+      item[e.target.id] = e.target.checked;
+    } else {
+      item[e.target.id] = e.target.value.trim();
+    }
     chrome.storage.sync.set(item, function() {
       // Update status to let user know options were saved.
       var status = document.getElementById('status');
@@ -21,17 +26,24 @@ function save(e) {
 function restore() {
   // here we can set defaults
   chrome.storage.sync.get({
-    cssurl: ''
+    cssurl: '',
+    cssremove: true
   }, function(items) {
     // we are looping through the different settings are assigning the value to the right elements
     for (var item in items) {
-      document.getElementById(item).value = items[item];
+      // handle checkboxes
+      if (typeof(items[item]) === 'boolean') {
+        document.getElementById(item).checked = items[item];
+      } else {
+        document.getElementById(item).value = items[item];
+      }
     }
   });
 }
 
-// event listener for the input
+// event listener for the inputs
 document.getElementById('cssurl').addEventListener('input', save);
+document.getElementById('cssremove').addEventListener('change', save);
 
 // load default settings when page is ready
 document.addEventListener('DOMContentLoaded', restore);
